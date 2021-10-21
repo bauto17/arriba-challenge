d758f637c27f63b6f7504064a1338465e18f5f22
sebastian <Lbautista.patarroyo@gmail.com>
2021-10-20 20:58:02 -0500

    change port

--- infraestructure/config/Config.go
+++ infraestructure/config/Config.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
 type GeneralConfig struct {
 	DatabaseConfig
 	WebServerConfig
@@ -8,16 +14,16 @@ type GeneralConfig struct {
 func GetConfig() GeneralConfig {
 	return GeneralConfig{
 		DatabaseConfig{
-			Host:               "localhost",
-			DbName:             "arriba",
-			Password:           "secret",
-			User:               "postgres",
-			Port:               5432,
+			Host:               getEnv("DB_HOST"),
+			DbName:             getEnv("DB_NAME"),
+			Password:           getEnv("DB_PASS"),
+			User:               getEnv("DB_USER"),
+			Port:               getIntEnv("DB_PORT"),
 			PoolSize:           20,
 			ConnLifeTimeSecond: 120,
 		},
 		WebServerConfig{
-			Port: ":8080",
+			Port: getEnv("PORT"),
 		},
 	}
 }
@@ -28,10 +34,28 @@ type WebServerConfig struct {
 
 type DatabaseConfig struct {
 	Host               string
-	Port               int64
+	Port               int
 	DbName             string
 	User               string
 	Password           string
 	PoolSize           int64
 	ConnLifeTimeSecond int64
+}
+
+func getEnv(envName string) string {
+	value := os.Getenv(envName)
+	if value == "" {
+		log.Fatalf("Wrong environment variable")
+	}
+
+	return value
+}
+
+func getIntEnv(envName string) int {
+	value := getEnv(envName)
+	result, err := strconv.Atoi(value)
+	if err != nil {
+		log.Fatalf("Wrong environment variable type. Expected '%s' of type int", envName)
+	}
+	return result
 }
